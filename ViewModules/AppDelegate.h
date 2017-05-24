23cc3221687802c9a9b4b7ae0df2f4b13fbfa55c
ztli <zitao.li@dianping.com>
2017-05-24 16:00:31 +0800

    add project

---
+++ ViewModules/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewModules
+//
+//  Created by lizitao on 2017/5/24.
+//  Copyright © 2017年 lizitao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
